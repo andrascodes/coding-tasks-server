f8debbae16fa4f2e9126884e9ab7c72923adde2d
Andras Szucs <andrew0szucs@gmail.com>
2019-10-24 13:39:23 +0200

    Initialize repository with Yarn and set up ESLint, Prettier, Jest with Typescript

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    es6: true,
+    node: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  rules: {
+    "no-console": "off",
+  },
+  settings: {
+    "import/resolver": {
+      node: {
+          paths: ["src"],
+          extensions: [".js", ".jsx", ".ts", ".tsx"]
+      }
+    }
+  }
+};
