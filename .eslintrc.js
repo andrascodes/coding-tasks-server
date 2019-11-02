bfd714ac11150e02e1e272b8ccd65376e9312a29
Andras Szucs <andrew0szucs@gmail.com>
2019-11-03 00:34:32 +0100

    Remove no-console rule from .eslintrc

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,30 +5,28 @@ module.exports = {
     jest: true,
   },
   extends: [
-    'airbnb-base',
-    'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "airbnb-base",
+    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
+    "prettier/@typescript-eslint", // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
+    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
   ],
   globals: {
-    Atomics: 'readonly',
-    SharedArrayBuffer: 'readonly',
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
   },
-  parser: '@typescript-eslint/parser',
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: 2018,
-    sourceType: 'module',
-    project: './tsconfig.json',
+    sourceType: "module",
+    project: "./tsconfig.json",
   },
-  rules: {
-    "no-console": "off",
-  },
+  rules: {},
   settings: {
     "import/resolver": {
       node: {
-          paths: ["src"],
-          extensions: [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
+        paths: ["src"],
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
 };
